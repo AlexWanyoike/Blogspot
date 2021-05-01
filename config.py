2f90fef5219635ef65f3a31692720ab4dfeb1fee
AlexWanyoike <alex.wanyoike12@gmail.com>
2021-05-01 21:11:18 +0300

    Views Created

---
+++ config.py
@@ -0,0 +1,42 @@
+
+import os
+
+class Config:
+    '''
+    General configuration parent class
+    '''
+    @staticmethod
+    def init_app(app):
+        pass
+
+
+
+class ProdConfig(Config):
+    '''
+    Production  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    pass
+
+
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+
+    DEBUG = True
+
+class TestConfig(Config):
+    pass
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig,
+'test': TestConfig
+
+}
---
+++ manage.py
@@ -0,0 +1,34 @@
+#from app import create_app
+#from app.models import User
+
+#from flask_migrate import Migrate, MigrateCommand
+#from flask_script import Manager,Shell,Server
+
+# Creating app instance
+#app = create_app('development')
+
+
+#manager = Manager(app)
+#manager.add_command('server',Server)
+
+#migrate = Migrate(app,db)
+#manager.add_command('db',MigrateCommand)
+
+#@manager.command
+#def test():
+ #   """Run the unit tests."""
+  #  import unittest
+   # tests = unittest.TestLoader().discover('tests')
+    #unittest.TextTestRunner(verbosity=2).run(tests)
+
+#@manager.shell
+#def make_shell_context():
+ #   return dict(app = app)
+
+#if __name__ == '__main__':
+ #   manager.run(debug=True)
+
+from app import app
+
+if __name__ == '__main__':
+    manager.run(debug = True)
